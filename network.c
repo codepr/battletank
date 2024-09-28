b08e7c0cd5be3d05271c4b4d09b65bb1f0e6eeff
Andrea Baldan <a.g.baldan@gmail.com>
2024-09-29 00:17:27 +0100

    Add networking utility helpers

--- network.c
+++ network.c
@@ -2,6 +2,8 @@
 
 #include <errno.h>
 
+#include "protocol.h"
+
 ssize_t network_send(int fd, const char *buf, size_t count) {
     ssize_t n = 0;
     size_t written = 0;
@@ -21,19 +23,43 @@ ssize_t network_send(int fd, const char *buf, size_t count) {
     return written;
 }
 
-ssize_t network_recv(int fd, char *buf, size_t count) {
-    ssize_t n = 0;
-    size_t received = 0;
-    /* Let's read from the client */
-    while (received < count) {
-        n = read(fd, buf + n, count);
+/**
+ * Receives a serialized packet from the network over a socket file descriptor.
+ *
+ * - Reads the first 4 bytes to determine the total length of the packet.
+ * - Reads data in a loop until the entire packet is received or an error
+ * occurs.
+ * - If a non-blocking socket is used, it will return immediately when no data
+ * is available (errno = EAGAIN or EWOULDBLOCK).
+ */
+ssize_t network_recv(int fd, char *buf) {
+    int received = 0, count = sizeof(int);
+
+    // Read the header length
+    ssize_t n = read(fd, buf, count);
+    if (n == -1) {
+        if (errno == EAGAIN || errno == EWOULDBLOCK)
+            goto exit;
+        else
+            return n;
+    }
+
+    int total_length = bin_read_i32(buf);
+    received += count;
+
+    // Read from the client the remaining packet
+    while (received < total_length) {
+        n = read(fd, buf + received, total_length - received);
         if (n == -1) {
             if (errno == EAGAIN || errno == EWOULDBLOCK)
                 break;
             else
                 return n;
         }
+
         received += n;
     }
+
+exit:
     return received;
 }
--- network.h
+++ network.h
@@ -4,7 +4,6 @@
 #include <unistd.h>
 
 ssize_t network_send(int fd, const char *buf, size_t count);
-
-ssize_t network_recv(int fd, char *buf, size_t count);
+ssize_t network_recv(int fd, char *buf);
 
 #endif
