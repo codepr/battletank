3ed76e62f3ad613a33968949a8389e5db7d2e5e8
Andrea Baldan <a.g.baldan@gmail.com>
2024-10-06 18:49:41 +0100

    Fixed bug, wrongly used char instead of unsigned char

--- network.h
+++ network.h
@@ -3,7 +3,7 @@
 
 #include <unistd.h>
 
-ssize_t network_send(int fd, const char *buf, size_t count);
-ssize_t network_recv(int fd, char *buf);
+ssize_t network_send(int fd, const unsigned char *buf, size_t count);
+ssize_t network_recv(int fd, unsigned char *buf);
 
 #endif
